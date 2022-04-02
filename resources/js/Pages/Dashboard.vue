cff23e8904731273b63bf1b23e2b0038eec595a5
Xavi Baz <fjbaz@hiberus.com>
2022-04-02 10:54:49 +0200

    Format table structure for data

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,9 +21,22 @@ const props = defineProps({
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <div v-for="order in orders" :key="order.id">
-                            {{ order.id_order }}
-                        </div>
+                        <table class="divide-y divide-gray-300">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th class="px-6 py-2 text-xs text-gray-500">Reference</th>
+                                    <th class="px-6 py-2 text-xs text-gray-500">Customer</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-300">
+                                <div v-for="order in orders" :key="order.id">
+                                    <tr class="whitespace-nowrap">
+                                        <td class="px-6 py-4 text-sm text-gray-500">{{ order.reference }}</td>
+                                        <td class="px-6 py-4 text-sm text-gray-500">{{ order.id_customer }}</td>
+                                    </tr>
+                                </div>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
